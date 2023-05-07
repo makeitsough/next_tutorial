c175a92867d28cdc7d61b7c5fde1fc6796bcee76
Paul Schofield <paul.schofield.us@gmail.com>
2023-05-07 16:29:56 -0400

    Render posts

--- models/prompt.js
+++ models/prompt.js
@@ -1,8 +1,8 @@
-import mongoose, { Schema, model, models } from 'mongoose';
+import { Schema, model, models } from 'mongoose';
 
 const PromptSchema = new Schema({
   creator: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: 'User',
   },
   prompt: {
@@ -11,7 +11,7 @@ const PromptSchema = new Schema({
   },
   tag: {
     type: String,
-    required: [true, 'Tag is required'],
+    required: [true, 'Tag is required.'],
   },
 });
 
